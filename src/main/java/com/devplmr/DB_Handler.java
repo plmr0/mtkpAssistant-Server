bf3ef60c22773b325f8006d6bdadc598037e256b
plmr0 <ilya.l2013@yandex.ru>
2019-03-28 23:58:24 +0300

    Create DB_Handler.java

---
+++ src/main/java/com/devplmr/DB_Handler.java
@@ -0,0 +1,91 @@
+package com.devplmr;
+
+import org.sqlite.JDBC;
+
+import java.io.File;
+import java.sql.*;
+
+public class DB_Handler
+{
+	public static final String DB_NAME = "assistant_db.sqlite3";
+
+	public DB_Handler() throws SQLException
+	{
+		DriverManager.registerDriver(new JDBC());
+
+		File folder = new File("sqlite");
+		if (!folder.exists()) folder.mkdir();
+		else {}
+	}
+
+	private Connection connect()
+	{
+		String url = "jdbc:sqlite:" + System.getProperty("user.dir") + "/sqlite/" + DB_NAME;
+		Connection conn = null;
+		try
+		{
+			conn = DriverManager.getConnection(url);
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e.getMessage());
+		}
+		return conn;
+	}
+
+	public static void createNewDatabase()
+	{
+		String url = "jdbc:sqlite:" + System.getProperty("user.dir") + "/sqlite/" + DB_NAME;
+
+		try (Connection conn = DriverManager.getConnection(url))
+		{
+			if (conn != null)
+			{
+				DatabaseMetaData meta = conn.getMetaData();
+			}
+			else {}
+
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public static void createNewGroupTable()
+	{
+		String url = "jdbc:sqlite:" + System.getProperty("user.dir") + "/sqlite/" + DB_NAME;
+
+		String sql = "CREATE TABLE groups\n"
+				+ "(\n"
+				+ "id INTEGER PRIMARY KEY,\n"
+				+ "group_name TEXT NOT NULL\n"
+				+ ");";
+
+		try (Connection conn = DriverManager.getConnection(url);
+		     Statement stmt = conn.createStatement())
+		{
+			stmt.execute(sql);
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public void insertGroups(String group_name)
+	{
+		String sql = "INSERT INTO groups(group_name) VALUES(?)";
+
+		try (Connection conn = this.connect();
+		     PreparedStatement preparedStatement = conn.prepareStatement(sql))
+		{
+			preparedStatement.setString(1, group_name);
+			preparedStatement.executeUpdate();
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+}
