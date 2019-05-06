4fc6a670b16818b7fd7f3a01dd5820f75e51ff71
plmr0 <ilya.l2013@yandex.ru>
2019-05-07 02:37:24 +0300

    VK Data Refactoring

--- src/main/java/com/devplmr/VKData.java
+++
@@ -1,8 +0,0 @@
-package com.devplmr;
-
-public class VKData
-{
-	public final static int appId = 0; 
-	public final static String accessToken = "";
-	public final static int groupId = 0;
-}
---
+++ src/main/java/com/devplmr/VK_Data.java
@@ -0,0 +1,9 @@
+package com.devplmr;
+
+public class VK_Data
+{
+	public static final int APP_ID = 0;
+	public static final String ACCESS_TOKEN = "";
+
+	public static final int GROUP_ID = 0;
+}
